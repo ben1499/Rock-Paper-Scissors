6a7ab6ca1ad01753589762abe35e5f19fd2dc237
Reuben <reubenthomas99@gmail.com>
2022-08-01 09:41:32 +0530

    Add function for computer choice

---
+++ script.js
@@ -0,0 +1,16 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3) + 1;
+    switch(choice)
+    {
+        case 1: console.log("Rock")
+                break;
+        case 2: console.log("Paper");
+                break;
+        case 3: console.log("Scissors");
+                break;
+        default: console.log("Wrong Choice");
+    }
+} 
+
+
+
