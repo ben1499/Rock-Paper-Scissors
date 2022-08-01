9aae402fcfbba57322072506e5caba13c8be7ba8
Reuben <reubenthomas99@gmail.com>
2022-08-01 15:23:21 +0530

    Add functionality to track winner

--- script.js
+++ script.js
@@ -24,48 +24,61 @@ function playRound(playerSelection, computerSelection) {
     //playerSelection = /playerSelection/i;
     //computerSelection = /computerSelection/i;
     let result;
+  
     if(playerSelection === 'rock' && computerSelection == 'scissors') {
         result = "You win! Rock beats Scissors";
-        
+        flag = 1;
     }
     else if(playerSelection === 'scissors' && computerSelection === 'paper') {
         result = "You win! Scissors beat Paper";
-        
+        flag = 1;
     }
     else if(playerSelection === 'paper' && computerSelection === 'rock') {
         result = "You win! Paper beats Rock";
-        
+        flag = 1;
     }
     else if(computerSelection === 'rock' && playerSelection === "scissors") {
         result = "You lose! Rock beats Scissors";
+        flag = 0;
     }
     else if(computerSelection === 'scissors' && playerSelection === 'paper') {
         result = "You lose! Scissors beat Paper";
-        
+        flag = 0;
     }
     else if(computerSelection === 'paper' && playerSelection === 'rock') {
         result = "You lose! Paper beats Rock";
-        
+        flag = 0
     }
     else {
         result = "It is a tie !";
     }
     return result;
-   
 }
 
 
 
 
 function game() {
+    let gameWin = 0;
+    let gameLose = 0;
     for(let i = 0; i < 5; i++)
     {   
         let playerSelection = prompt("Enter a choice");
         let computerSelection = getComputerChoice();
         console.log(playRound(playerSelection, computerSelection));
+        if(flag === 1)
+           gameWin++; 
+        else if(flag === 0)
+            gameLose++;
     }
+    if(gameWin > gameLose)
+        console.log("Congrats! You win");
+    else if(gameLose > gameWin)
+        console.log("You lose Try again !");
+    else
+        console.log("It's a draw !")
 }
 
-
+let flag;
 game();
 
